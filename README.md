7586d84aa67ba627bf3659c793a9067c23726b57
Simbiat <simbiat@outlook.com>
2021-03-24 16:49:04 +0300

    Added actual content

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # HTMLCache
-Yes, this is a caching library. Yes, I am aware of Symfony/Cache and PSR-6/PSR-16. But I've realised, there is a flaw in those approaches, if we are to provide a consistent web-pages. THis library will try to remidiate this. Meanwhile, since one of my goals is to depend on as few external libraries as possible, there will be no dependencies on mentioned libraries, although, some ideas may be taken from them.
+This is a caching library designed specifically for caching of web-pages. It is not compliant with [PSR-6](https://github.com/php-fig/cache), in terms of format, although some functionallity may be similar. It's not meant as a replacement for other caching solutions like Symfony's [Cache](https://github.com/symfony/cache), as well, sicne it's a niche solution, rather a more generic one.
+
+## Why?
+While caching seems to be long-resolved thing with lots of good solutions, I've found 2 points, that did not sit right with my desire to server consistent web-pages:
+1. If codebase serving a page changed, it did not invalidate the cached version of a page automatically, meaning a chance to serve stale content.
+2. If I am to serve identical headers, I would need to cache them separately and link them somehow to the cached content through extra logic.
+Some may say that these are minor issues and he may be right, that they are unlikely to cause an actual issue, but for me both mean that unless I add extra logic acompanying Symfony\Cache (used previously), I will serve stale content with new headers, that can worsen user experience. I do not want that.
+
+## What?
+That's why I've decided to write my own solution, designed specifcially for web pages.  
+First problem is solved by calculating the version of the codebased like this:
+```php
+$usedFiles = get_included_files();
+$this->version = count($usedFiles).'.'.max(max(array_map('filemtime', array_filter($usedFiles, 'is_file'))), 
+```
+And then this version is stored in the cache for each designated key. When getting the data out of cache it will be matched with current one and if they do not match, it will be treated as a cache miss. This is not a 100% guarantee, but since I have a maing "gateway" file that handles all the page generation, it works for me just fine.  
+Second problem is solved by caching headers sent along with the data. Once the data is retrived the same headers will be sent once again.  
+But that's not all: it has other features, that may be useful:
+1. Optional zipping of the data in cache to reduce footprint
+2. Direct output using `zEcho` function from [HTTP20](https://github.com/Simbiat/HTTP20/blob/main/doc/Common.md#zecho) library, if available (if not regular `echo` will be used)
+3. Validation of cache using hash to reduce chances of serving corrupted cache
+4. Techniques to negate cache-slamming (at least, to an extent)
+5. Automatically tries to use either APCU or filestorage. Memcache and the like are not supported, since I do not ahve appropriate infrastructure to test them.
+6. Transient in case of failures so that your webpage will still be displayed (generally)
+
+## How?
+Here's a simple example of how I'm using it. Scroll further for more details on the functions.
+```php
+#Create HTMLCache object
+$HTMLCache = (new \Simbiat\HTMLCache($siteconfig['cachedir'].'html/'));
+#Attempt to use cache
+$HTMLCache->get('', true, true);
+#Do some processing in case cache was not hit, to get $output
+#Save to cache and output directly
+if ($uri[1] === 'statistics') {
+    $HTMLCache->set($output, '', 604800, 600, true, true);
+} elseif ($uri[1] === 'achievement') {
+    $HTMLCache->set($output, '', 259200, 600, true, true);
+}
+#Output page if not requried to cache
+(new \Simbiat\http20\Common)->zEcho($output);
+```
+
+## Details on usage
+### Construct
+```php
+_construct(string $filesPool = '', int $maxRandom = 60)
+```
+When creating the object you can specify path where files of the cache will be stored using `$filesPool`. If empty, this will let the class know, that you do not want to use filestorage for caching.  
+In order to negate cach slamming, class reduces expiration date during validation by a random amount from 0 to `$maxRandom`, which is defaulted to 60 seconds. You can adjust this number or use 0 to, essentially, disable this feature (not advisable).  
+
+### Set
+```php
+set(string $string, string $key ='', int $ttl = 600, int $grace = 600, bool $zip = true, bool $direct = true, string $cacheStrat = '')
+```
+Use `set` to write to cache. `$string` is the only mandatory value. Since the class is designed for HTML pages, we are restricting the type of the value to `string` only.  
+`$key` is an optional value for ID with which the value will be stored. If empty current `REQUEST_URI` will be used (if it's empty `index.php` will be used). Regardless, the value will be hashed for consistency.  
+`$ttl` is `time to live` for the cached value. After it expires, the value will be considered `stale`. Defaults to 600, that is 10 minutes.  
+`$grace` is an optional grace period to help with cache slamming. When cache hit is successful, but it has expired, class updates the expiration value to `time()+$grace` and sets `$grace = 0`. This helps with concurrent requests, so that they will still recieve the stale data for extra seconds after its expiration, while initial hit updates the cache.  
+`$zip` will GZIP the body and headers of the page to save some space. With current processing power and average size of HTML pages, this is a very fast operation, which can help you cache more stuff both in memory and on disk. But you can disable it, if you want, by setting it to `false`.  
+`$direct`, if set to `true` will output the webpage right after cache is written. Since we are dealing with webpages, there is not much reason to do something after we have a generated page, but you can disable this behaviour and, instead, recieve a boolean value representing the result of the function.  
+`$cacheStrat` is used for setup of `Cache-Control` header (using appropriate [function](https://github.com/Simbiat/HTTP20/blob/main/doc/Headers.md#cachecontrol)) if you are using `zEcho`. This value will also be cached.
+
+### Get
+```php
+get(string $key = '', bool $scriptVersion = true, bool $direct = true)
+```
+Use `get` to get the cached data.
+`$key` is an optional value for ID with which the value will be stored. If empty current `REQUEST_URI` will be used (if it's empty `index.php` will be used). Regardless, the value will be hashed for consistency.  
+`$scriptVersion`, if set to `true`, will force validation of codebase version, as described above. Since this is more of a personal preference, you can disable this feature.  
+`$direct`, if set to `true` will output the webpage right getting the page. Since we are dealing with webpages, there is not much reason to disable this, but you can do this and, instead, recieve an array of representing all the cached data. I doubt it will be useful outside of the class, though.  
+
+### Delete
+```php
+delete(string $key = '')
+```
+Use `delete` to remove cached item.  
+`$key` is an optional value for ID with which the value will be stored. If empty current `REQUEST_URI` will be used (if it's empty `index.php` will be used). Regardless, the value will be hashed for consistency.
--- src/HTMLCache.php
+++ src/HTMLCache.php
@@ -4,6 +4,254 @@ namespace Simbiat;
 
 class HTMLCache
 {
+    #Settings initialized on construction
+    private string $version = '';
+    private bool $apcu = false;
+    private string $files = '';
+    private bool $poolReady = false;
+    private bool $zEcho = false;
+    private int $maxRandom = 60;
     
+    public function __construct(string $filesPool = '', int $maxRandom = 60)
+    {
+        #Sanitize random value
+        if ($maxRandom < 0) {
+            $maxRandom = 60;
+        }
+        $this->maxRandom = $maxRandom;
+        #Get version of the scripts based on all files called so far
+        $usedFiles = get_included_files();
+        $this->version = count($usedFiles).'.'.max(max(array_map('filemtime', array_filter($usedFiles, 'is_file'))), getlastmod());
+        #Check if APCU is available
+        if (extension_loaded('apcu') && ini_get('apc.enabled')) {
+            $this->apcu = true;
+        }
+        #Check if filebased pool exists
+        if (!empty($filesPool)) {
+            if (is_dir($filesPool)) {
+                $this->files = rtrim(rtrim($filesPool, '\\'), '/').'/';
+            } else {
+                #If it does not exist, attempt to create it
+                if (mkdir($filesPool, recursive: true)) {
+                    $this->files = rtrim(rtrim($filesPool, '\\'), '/').'/';
+                }
+            }
+        }
+        #If either APCU or files pooling are available - set the flag to true
+        if ($this->files !== '' || $this->apcu === true) {
+            $this->poolReady = true;
+        }
+        #Check if zEcho is available
+        if (method_exists('\Simbiat\http20\Common', 'zEcho')) {
+            $this->zEcho = true;
+        }
+    }
+    
+    #Function to store HTML page
+    public function set(string $string, string $key ='', int $ttl = 600, int $grace = 600, bool $zip = true, bool $direct = true, string $cacheStrat = ''): bool
+    {
+        if ($this->poolReady) {
+            #Sanitize integers
+            if ($ttl < 1) {
+                $ttl = 600;
+            }
+            if ($grace < 1) {
+                $grace = 600;
+            }
+            #Set key based on REQUEST_URI
+            if (empty($key)) {
+                $key = hash('sha3-256', (empty($_SERVER['REQUEST_URI']) ? 'index.php' : $_SERVER['REQUEST_URI']));
+            } else {
+                $key = hash('sha3-256', $key);
+            }
+            #GZip data
+            if ($zip && extension_loaded('zlib')) {
+                $body = gzcompress($string, 9, FORCE_GZIP);
+                $headers = gzcompress(serialize(headers_list()), 9, FORCE_GZIP);
+            } else {
+                $body = $string;
+                $headers = headers_list();
+                $zip = false;
+            }
+            #Organize HTTP data
+            $data = [
+                'body' => $body,
+                'headers' => $headers,
+            ];
+            #Organize data for storage
+            $data = [
+                'key' => $key,
+                'zip' => $zip,
+                'version' => $this->version,
+                'cacheStrat' => $cacheStrat,
+                'uri' => $_SERVER['REQUEST_URI'],
+                'data' => $data,
+            ];
+            #Hash the data
+            $data['hash'] = hash('sha3-256', serialize($data));
+            #Set timings
+            $data['expires'] = time()+$ttl;
+            $data['grace'] = $grace;
+            #Write cache
+            $result = $this->writeToCache($key, $data);
+        } else {
+            $result = false;
+        }
+        #Echo the data if we chosed to do it
+        if ($direct) {
+            if ($this->zEcho) {
+                (new \Simbiat\http20\Common)->zEcho($string, $cacheStrat);
+            } else {
+                echo $string;
+                exit;
+            }
+        } else {
+            return $result;
+        }
+    }
+    
+    #Function to get HTML page from cache
+    public function get(string $key = '', bool $scriptVersion = true, bool $direct = true): bool|array
+    {
+        if ($this->poolReady) {
+            #Set key based on REQUEST_URI
+            if (empty($key)) {
+                $key = hash('sha3-256', (empty($_SERVER['REQUEST_URI']) ? 'index.php' : $_SERVER['REQUEST_URI']));
+            } else {
+                $key = hash('sha3-256', $key);
+            }
+            #Check APCU
+            if ($this->apcu && apcu_exists($key) === true) {
+                #Get data from cache
+                $data = apcu_fetch($key, $result);
+                #Check that data was retrieved. If not we will fall through to file.
+                if ($result === false) {
+                    $data = NULL;
+                }
+            }
+            #Check file
+            if (empty($data) && $this->files !== '' && is_file($this->files.$key) && is_readable($this->files.$key)) {
+                $data = unserialize(file_get_contents($this->files.$key));
+            }
+            #Validate data
+            if (empty($data)) {
+                return false;
+            } else {
+                if ($this->cacheValidate($key, $data, $scriptVersion) === true) {
+                    #Output data
+                    if ($direct) {
+                        $this->cacheOutput($data);
+                    } else {
+                        return $data;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    #Function to remove from cache
+    public function delete(string $key = ''): bool
+    {
+        #Sanitize key
+        if (empty($key)) {
+            $key = hash('sha3-256', (empty($_SERVER['REQUEST_URI']) ? 'index.php' : $_SERVER['REQUEST_URI']));
+        } else {
+            $key = hash('sha3-256', $key);
+        }
+        #Remove from APCU
+        if ($this->apcu && apcu_exists($key) === true) {
+            $result = apcu_delete($key);
+            if (!$result) {
+                return false;
+            }
+        }
+        #Remove file
+        if ($this->files !== '' && is_file($this->files.$key)) {
+            $result = unlink($this->files.$key);
+            if (!$result) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    #Helper function to write cache data
+    private function writeToCache(string $key, array $data): bool
+    {
+        #Cache data to APCU
+        if ($this->apcu) {
+            $result = apcu_store($key, $data);
+            if (!$result) {
+                return false;
+            }
+        }
+        #Cache data to file
+        if ($this->files !== '') {
+            $result = boolval(file_put_contents($this->files.$key, serialize($data), LOCK_EX));
+            if (!$result) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    #Helper function to validate cache
+    private function cacheValidate(string $key, array $data, bool $scriptVersion = true): bool
+    {
+        #Check key
+        if ($key !== $data['key']) {
+            return false;
+        }
+        #Check if stale. We use a random seed to allow earlier expiration, which can help with cache slamming
+        if (empty($data['expires']) || $data['expires'] < time() - rand(0, $this->maxRandom)) {
+            #Check grace period
+            if (empty($data['grace'])) {
+                return false;
+            } else {
+                #Prepare new set of data
+                $newdata = $data;
+                $newdata['expires'] = time()+$newdata['grace'];
+                $newdata['grace'] = 0;
+            }
+        }
+        #Check script version. This may help avoid situations, when you have updated PHP files, that are responsible for page generation, but there is also a cache version, which uses older revisions, that may provide inappropriate results
+        if ($scriptVersion && $this->version !== $data['version']) {
+            return false;
+        }
+        #Check hash to reduce chances of serving corrupted data
+        $hash = $data['hash'];
+        unset($data['expires'], $data['grace'], $data['hash']);
+        if ($hash !== hash('sha3-256', serialize($data))) {
+            return false;
+        } else {
+            if (isset($newdata)) {
+                #Update expiration date in cache to prevent cache slamming
+                $this->writeToCache($key, $newdata);
+                #Return false in order to get up-to-date data
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+    
+    #Helper function to output cached data
+    private function cacheOutput(array $data): void
+    {
+        #Unzip data
+        if ($data['zip'] === true) {
+            $data['data']['body'] = gzdecode($data['data']['body']);
+            $data['data']['headers'] = unserialize(gzdecode($data['data']['headers']));
+        }
+        #Send headers
+        array_map('header', $data['data']['headers']);
+        if ($this->zEcho) {
+            (new \Simbiat\http20\Common)->zEcho($data['data']['body'], (empty($data['cacheStrat']) ? '' : $data['cacheStrat']));
+        } else {
+            echo $string;
+            exit;
+        }
+    }
 }
 ?>
